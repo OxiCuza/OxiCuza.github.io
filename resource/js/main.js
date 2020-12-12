a8be958249979a3b1f99892bac3a961e3895f8a3
Oxi_Cuza <oxicusa@gmail.com>
2020-12-12 21:42:54 +0700

    add slick slider and styling it

---
+++ resource/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.portfolio-slider').slick({
+        dot: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        cssEase: 'ease',
+        pauseOnHover: false,
+        // pauseOnFocus: false
+    });
+})
